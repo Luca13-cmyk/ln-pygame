7e58afe65206f0cce68ce456053e257032a67408
Luca <lucanegresco@gmail.com>
2024-03-17 20:24:38 -0300

    moving player1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame import Surface
+from pygame import Surface, Rect
 
 # Formatacao: CTRL + ALT + L
 
@@ -17,35 +17,41 @@ window: Surface = pygame.display.set_mode(size=(W_WIDTH, W_HEIGHT))
 bg_surf: Surface = pygame.image.load('./asset/bg.png').convert_alpha()
 player1_surf: Surface = pygame.image.load('./asset/player1.png').convert_alpha()
 
-# Desenhar na janela
-window.blit(source=bg_surf, dest=(0, 0))
-window.blit(source=player1_surf, dest=(100, 100))
+# Obter o retangulo da superficie
+bg_rect: Rect = bg_surf.get_rect(left=0, top=0)
+player1_rect: Rect = player1_surf.get_rect(left=100, top=100)
+
+# Desenhar na janela (window)
+window.blit(source=bg_surf, dest=bg_rect)
+window.blit(source=player1_surf, dest=player1_rect)
 
 # Atualizar a janela
 pygame.display.flip()
 print("setup end")
+
+# Colocar um relogio no jogo
 clock = pygame.time.Clock()
 running = True
+
 print("loop start")
 while running:
-    # # poll for events
-    # # pygame.QUIT event means the user clicked X to close your window
+    clock.tick(140)  # esse loop esta acontecendo 140 vezes por segundo
+    # print(f'{clock.get_fps() :.0f}')  # executar o print do fps
+    window.blit(source=bg_surf, dest=bg_rect)
+    window.blit(source=player1_surf, dest=player1_rect)
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("loop end")
             running = False
-    pass
-
-    # # fill the screen with a color to wipe away anything from last frame
-    # screen.fill("purple")
-    #
-    # # RENDER YOUR GAME HERE
-    #
-    # # flip() the display to put your work on screen
-    # pygame.display.flip()
-    #
-    # clock.tick(60)  # limits FPS to 60
+    pressed_key = pygame.key.get_pressed()
+    if pressed_key[pygame.K_w]:
+        player1_rect.centery -= 1
+    if pressed_key[pygame.K_s]:
+        player1_rect.centery += 1
+    if pressed_key[pygame.K_d]:
+        player1_rect.centerx += 1
+    if pressed_key[pygame.K_a]:
+        player1_rect.centerx -= 1
 
 pygame.quit()
-
-# done
