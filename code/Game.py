c3a8dbf5176bb5cdef7fbb735f3c6334c73c3a90
Luca <lucanegresco@gmail.com>
2024-03-18 12:35:57 -0300

    skeleto inicial

--- code/Game.py
+++ code/Game.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import pygame
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
 
