aad568de087868ace16ceaae737c30a2cb5f6088
Luca <lucanegresco@gmail.com>
2024-03-19 17:08:33 -0300

    clock

--- code/Level.py
+++ code/Level.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame
-from pygame import Surface
+import sys
 
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import COLOR_WHITE
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -16,8 +20,24 @@ class Level:
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
     def run(self):
+        # pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        # pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
+
         while True:
+            clock.tick(60)  # O while vai rodar 60x por segundo
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            self.level_text(14, f'fps: {clock.get_fps() : .0f}', COLOR_WHITE, (10, 10))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
             pygame.display.flip()  # Atualize a tela
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
