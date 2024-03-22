b973cfcf76b056b7b3b2987ae2f268cb38ae7c60
Luca <lucanegresco@gmail.com>
2024-03-22 18:02:32 -0300

    shoot

--- code/Const.py
+++ code/Const.py
@@ -28,7 +28,9 @@ ENTITY_SPEED = {'Level1Bg0': 0,
                 'Level1Bg5': 5,
                 'Level1Bg6': 6,
                 'Player1': 3,
+                'Player1Shot': 2,
                 'Player2': 3,
+                'Player2Shot': 3,
                 'Enemy1': 2,
                 'Enemy2': 1,
                 }
@@ -41,6 +43,8 @@ ENTITY_HEALTH = {'Level1Bg0': 999,
                  'Level1Bg6': 999,
                  'Player1': 300,
                  'Player2': 300,
+                 'Player1Shot': 1,
+                 'Player2Shot': 1,
                  'Enemy1': 200,
                  'Enemy2': 200,
                  }
@@ -60,4 +64,7 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
 
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d
+                    }
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_LCTRL,
+                    'Player2': pygame.K_RCTRL
                     }
--- code/Level.py
+++ code/Level.py
@@ -8,9 +8,11 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 from code.Const import COLOR_WHITE, MENU_OPTION, EVENT_ENEMY
+from code.Enemy import Enemy
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 from code.EntityMediator import EntityMediator
+from code.Player import Player
 
 
 class Level:
@@ -37,6 +39,10 @@ class Level:
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+                if isinstance(ent, (Player, Enemy)):
+                    shoot = ent.shoot()
+                    if shoot is not None:
+                        self.entity_list.append(shoot)
 
             # Texto a ser printado na tela
             self.level_text(14, f'fps: {clock.get_fps() : .0f}', COLOR_WHITE, (10, 10))
